17fbc74edac7199dab0ad158eb49357a25f6eed1
hunderboy <wheeing@naver.com>
2021-01-28 19:13:27 +0900

    // 스톱워치 + progress bar

--- app/src/main/java/kr/co/everex/progressbarexample/MainActivity.kt
+++ app/src/main/java/kr/co/everex/progressbarexample/MainActivity.kt
@@ -2,39 +2,23 @@ package kr.co.everex.progressbarexample
 
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.*
 import kr.co.everex.progressbarexample.databinding.ActivityMainBinding
+import java.util.*
+import kotlin.system.measureTimeMillis
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
-
-    var total = 0 // time
-
-
-
-
-
-    fun ready(){
-        val readyToProgressBar = scope.launch {
-            for (i in 1..500) { // 1 to 500
-
-                binding.progressBarHorizonLine.secondaryProgress = binding.progressBarHorizonLine.secondaryProgress +1 // i는 500까지 감
-
-                delay(10) // 0.01 millisecond
-                if(i == 500) {
-                    Toast.makeText(applicationContext, "i = $i", Toast.LENGTH_SHORT).show()
-                    // 운동 핸들러 시작
-                    binding.progressBarHorizonLine.max = 1000 // 50초 = 5000 칸
-                    binding.progressBarHorizonLine.secondaryProgress = 0 // 0으로 초기화
-                    play()
-                }
-            }
-        }
-        readyToProgressBar.isActive
-    }
+    // 스톱워치 변수
+    private var time = 0
+    private var isRunning = false
+    private var timerTask: Timer? = null
+    private var progressNumber = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,32 +27,58 @@ class MainActivity : AppCompatActivity() {
         setContentView(view) // 뷰 바인딩 적용 완료
 
 
-        binding.button.setOnClickListener{
-
+        // 일시정지 상태에서는 데이터 정지 + 일시정지 화면을 띄운다.
+        binding.fabStart.setOnClickListener{
+            isRunning = !isRunning
+            if (isRunning) start() else pause()
         }
-
-        // 실제로 5초의 휴식 시간 이 꼭 무조건 적으로 정확해야 하는가?
-        // 화면 안정화 상태에 접어든 뒤에 실행시키면, 실제로 5초동안 정확히 동작 하는가?
-
-
-//        binding.progressBarHorizonLine.progress = total
-//
-//        val oneMin = 1 * 60 * 1000 // 1 minute in milli seconds
-//
-//        /** CountDownTimer starts with 1 minutes and every onTick is 1 second */
-//        cdt = object : CountDownTimer(
-//            oneMin.toLong(), 1000
-//        ) {
-//            override fun onTick(millisUntilFinished: Long) {
-//                total = (timePassed / 60 * 100) as Int
-//                binding.progressBarHorizonLine.progress = total
-//            }
-//            override fun onFinish() {
-//                // DO something when 1 minute is up
-//            }
-//        }.start()
-
+        binding.fabReset.setOnClickListener{
+            reset()
+        }
     }
+
+    // 시작 함수
+    private fun start() {
+        binding.fabStart.text = "일시정지" // 텍스트 일시정지로 변경
+
+        // period = 10 는 무엇을 뜻하는 것인가?
+        timerTask = kotlin.concurrent.timer(period = 10) {
+            time++
+            progressNumber++
+            val sec = time / 100
+            val milli = time % 100 // 나머지 값 : 00 ~ 99
+
+
+            // 지켜보고 있다가 데이터가 변하면 바로 적용
+            runOnUiThread {
+                binding.secText.text = "$sec"
+                binding.milliText.text = "$milli"
+                binding.progressBarHorizonLine.secondaryProgress = progressNumber // 0 ~ 500
+
+
+            }
+        }
+    }
+
+    // 일시정지 함수
+    private fun pause() {
+        binding.fabStart.text = "시작" // 텍스트 시작으로 변경
+        timerTask?.cancel()
+    }
+
+    private fun reset() {
+        timerTask?.cancel()
+
+        binding.progressBarHorizonLine.secondaryProgress = 0
+        progressNumber = 0
+
+        time = 0
+        isRunning = false
+        binding.fabStart.text = "시작"
+        binding.secText.text = "0"
+        binding.milliText.text = "00"
+    }
+
 
 
 }
