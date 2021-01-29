7a05720e490329c76f104382d72d1d59edfea2b8
hunderboy <wheeing@naver.com>
2021-01-29 15:41:09 +0900

    // secondary 진행 후 progress 진행은 시켰음. 근데 그 사이에 딜레이가 존재(아마 max = 1000 값으로 바꾸는 상황에서 일어나는거 같음)

--- app/src/main/java/kr/co/everex/progressbarexample/MainActivity.kt
+++ app/src/main/java/kr/co/everex/progressbarexample/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.appcompat.app.AppCompatActivity
 import kotlinx.coroutines.*
 import kr.co.everex.progressbarexample.databinding.ActivityMainBinding
 import java.util.*
+import kotlin.properties.Delegates
 import kotlin.system.measureTimeMillis
 
 
@@ -18,7 +19,32 @@ class MainActivity : AppCompatActivity() {
     private var time = 0
     private var isRunning = false
     private var timerTask: Timer? = null
-    private var progressNumber = 0
+//    private var sec = 0
+//    private var milli = 0
+
+    // secondary = true
+    // progress = false
+    private var progressBooleanType = true
+
+
+    // 싱글톤
+    companion object {
+        // 싱글톤 object
+        val timeData = TimeData()
+    }
+
+    // 내부 클래스
+    class TimeData {
+        // 클래스 속성(second, millisecond)
+        var timeDataProgressValue = 0
+        var sec = 0
+        var milli: Int by Delegates.observable(0, {props, old, new ->
+            // 데이터 변경 시에 실행되는 코드
+            timeDataProgressValue += 1
+        })
+    }
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,26 +63,56 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+
     // 시작 함수
     private fun start() {
         binding.fabStart.text = "일시정지" // 텍스트 일시정지로 변경
 
         // period = 10 는 무엇을 뜻하는 것인가?
         timerTask = kotlin.concurrent.timer(period = 10) {
-            time++
-            progressNumber++
-            val sec = time / 100
-            val milli = time % 100 // 나머지 값 : 00 ~ 99
+            time++ // 계속 변경됨
+            // 변경 될때 마다 새롭게 변수 생성되고 연산되고, 연산된 변수 할당됨
+            timeData.sec = time / 100
+            timeData.milli = time % 100 // 나머지 값 : 00 ~ 99
 
+            /**
+             * sec = 5 가 되는 순간
+             * secondaryProgress = 0 초기화
+             * Progress +1 씩 증가
+             */
+             if(timeData.sec == 5){
+                 binding.progressBarHorizonLine.secondaryProgress = 0 // 칸 초기화
+                 binding.progressBarHorizonLine.max = 1000 // 1000 칸으로
+                 timeData.timeDataProgressValue = 0
+                 progressBooleanType = false
+             }
+            /**
+             * sec = 15초 가 되는 순간
+             * Progress = 0 초기화
+             * timerTask 중지
+             */
+            else if (timeData.sec == 15){
+                 timeData.timeDataProgressValue = 0
+                 progressBooleanType = true
+                 timerTask?.cancel() // 테스크 종료
+            }
 
             // 지켜보고 있다가 데이터가 변하면 바로 적용
             runOnUiThread {
-                binding.secText.text = "$sec"
-                binding.milliText.text = "$milli"
-                binding.progressBarHorizonLine.secondaryProgress = progressNumber // 0 ~ 500
+                binding.secText.text = "${timeData.sec}"
+                binding.milliText.text = "${timeData.milli}"
 
-
+                // Todo true
+                if(progressBooleanType){ // progressbar secondaryProgress 진행
+                    binding.progressBarHorizonLine.secondaryProgress =
+                        timeData.timeDataProgressValue
+                // Todo false
+                }else { // progressbar Main Progress 진행
+                    binding.progressBarHorizonLine. progress =
+                        timeData.timeDataProgressValue
+                }
             }
+
         }
     }
 
@@ -69,8 +125,12 @@ class MainActivity : AppCompatActivity() {
     private fun reset() {
         timerTask?.cancel()
 
+        binding.progressBarHorizonLine.max = 500 // 500 칸으로
+
+
+        binding.progressBarHorizonLine.progress = 0
         binding.progressBarHorizonLine.secondaryProgress = 0
-        progressNumber = 0
+        timeData.timeDataProgressValue = 0
 
         time = 0
         isRunning = false
